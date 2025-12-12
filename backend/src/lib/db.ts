257420651b86b7cafda5e26498f32598a36a82b6
jpnortonwastaken <norton.jo@northeastern.edu>
2025-12-12 16:09:42 -0500

    Database code fix

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -6,11 +6,7 @@ import { PrismaClient } from '@shared/db/src'
 
 const dialect = new PostgresDialect({
   pool: new Pool({
-    host: config.db.host,
-    port: config.db.port,
-    user: config.db.user,
-    password: config.db.password,
-    database: config.db.database,
+    connectionString: config.databaseUrl,
     max: 20,
   }),
 })
